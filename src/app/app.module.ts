b967d16ba55d8c4765b84774c5967539f2a1eb6f
Saglai <power1800w@gmail.com>
2023-09-19 18:55:25 +0700

    feat: add header component, routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,11 @@ import { AppComponent } from './app.component';
 import { MainPageComponent } from './pages/main-page/main-page.component';
 import { MoodWidgetComponent } from './components/mood-widget/mood-widget.component';
 import { TextBannerComponent } from './components/text-banner/text-banner.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { MoodTablePagesComponent } from './pages/mood-table-pages/mood-table-pages.component';
+import { MoodListComponent } from './components/mood-list/mood-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MoodPipe } from './pipes/mood.pipe';
 
 @NgModule({
   declarations: [
@@ -13,10 +18,15 @@ import { TextBannerComponent } from './components/text-banner/text-banner.compon
     MainPageComponent,
     MoodWidgetComponent,
     TextBannerComponent,
+    HeaderComponent,
+    MoodTablePagesComponent,
+    MoodListComponent,
+    MoodPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
