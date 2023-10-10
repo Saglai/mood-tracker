ddae8998420351d8b9360cc6e971f28e9f66fa88
Saglai <power1800w@gmail.com>
2023-10-10 21:23:29 +0700

    fix: all selected columns were sorted, but only one should be

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -7,31 +7,50 @@ import { MoodModel } from 'src/app/models/mood.model';
   styleUrls: ['./table.component.css']
 })
 export class TableComponent {
-  isMoodSortByRise = -1;
-  isDateSortByRise = -1;
+  shouldSortByRise!: Boolean;
+  currentSortedColumn!: String;
 
   constructor() {}
 
-  @Input() moodPosts!: MoodModel[];
+  @Input() posts!: MoodModel[];
 
   sortByMood(): void {
-    if (this.isMoodSortByRise === 0) {
-      this.moodPosts = this.moodPosts.sort((a, b) => +a.moodType - +b.moodType);
-      this.isMoodSortByRise = 1;
-    } else {
-      this.moodPosts = this.moodPosts.sort((a, b) => +b.moodType - +a.moodType);
-      this.isMoodSortByRise = 0;
+    const sortByRise = () => {
+      this.posts = this.posts.sort((a, b) => +a.moodType - +b.moodType);
     }
+
+    const sortByDecrease = () => {
+      this.posts = this.posts.sort((a, b) => +b.moodType - +a.moodType);
+    }
+
+    this.sortByColumn('mood', sortByRise, sortByDecrease);
   }
 
   sortByDate() {
-    if (this.isDateSortByRise === 0) {
-      this.moodPosts = this.moodPosts.sort((a, b) => this.toTimestamp(a.createdDate) - this.toTimestamp(b.createdDate));
-      this.isDateSortByRise = 1;
-    } else {
-      this.moodPosts = this.moodPosts.sort((a, b) => this.toTimestamp(b.createdDate) - this.toTimestamp(a.createdDate));
-      this.isDateSortByRise = 0;
+    const sortByRise = () => {
+      this.posts = this.posts.sort((a, b) => this.toTimestamp(a.createdDate) - this.toTimestamp(b.createdDate));
     }
+
+    const sortByDecrease = () => {
+      this.posts = this.posts.sort((a, b) => this.toTimestamp(b.createdDate) - this.toTimestamp(a.createdDate));
+    }
+
+    this.sortByColumn('date', sortByRise, sortByDecrease);
+  }
+
+  sortByColumn(col: string, sortByRiseFn: Function, sortByDecreaseFn: Function) {
+    if (this.currentSortedColumn !== col) {
+      this.currentSortedColumn = col;
+      this.shouldSortByRise = true;
+    }
+
+    if (this.shouldSortByRise) {
+      sortByRiseFn();
+    } else {
+      sortByDecreaseFn();
+    }
+
+    this.shouldSortByRise = !this.shouldSortByRise;
   }
 
   toTimestamp(time: String) {
